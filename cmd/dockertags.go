73ef3d677b10146a75d20ccd5d38c08cac1cab30
Marco Miranda <marco.miranda@1global.com>
2025-04-13 19:28:12 +0200

    Add functionality to list Docker tags in dockertags command

--- cmd/dockertags.go
+++ cmd/dockertags.go
@@ -1,6 +1,10 @@
 package cmd
 
 import (
+	"context"
+	"fmt"
+
+	"github.com/macmiranda/dockertags/pkg/docker"
 	"github.com/spf13/cobra"
 )
 
@@ -13,12 +17,28 @@ var rootCmd = &cobra.Command{
 	Use:   "dockertags",
 	Short: "A tool to manage Docker tags",
 	Long:  `A tool to manage Docker tags and their metadata`,
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		repository := args[0]
+		if debug {
+			fmt.Printf("Registry: %s\n", registry)
+			fmt.Printf("Repository: %s\n", repository)
+		}
+		client := docker.NewClient(debug)
+		tags, err := client.ListTags(context.Background(), registry, repository)
+		if err != nil {
+			return fmt.Errorf("failed to list tags: %v", err)
+		}
+		for _, tag := range tags {
+			fmt.Println(tag)
+		}
+		return nil
+	},
 }
 
 func init() {
-	rootCmd.Flags().StringVarP(&registry, "registry", "r", "", "Docker registry URL (e.g., docker.io, gcr.io)")
+	rootCmd.Flags().StringVarP(&registry, "registry", "r", "docker.io", "Docker registry URL (e.g., docker.io, gcr.io)")
 	rootCmd.Flags().BoolVar(&debug, "debug", false, "Enable debug output")
-	rootCmd.MarkFlagRequired("registry")
 }
 
 func Execute() error {
